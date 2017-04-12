4fe0370b0b7c502cc2473a79543b034c46b40b1a
Pyhro36 <pylou_11@hotmail.fr>
2017-04-12 12:18:12 +0200

    builder json format input -> output

--- projects/IHM/Build XML/builder.js
+++ projects/IHM/Build XML/builder.js
@@ -1,68 +1,57 @@
-var XMLWriter = require('xml-writer');
- 
-xw = new XMLWriter('\t');
-xw.startDocument('1.0', 'UTF-8');
-xw.startElement('fet');
-xw.writeAttribute('version', 'TODO value');
-xw.startElement('Institution_Name');
-xw.endElement('Institution_Name');
-xw.startElement('Comments');
-xw.text('Default comments');
-xw.endElement('Comments');
-xw.startElement('Days_List').startElement('Number_of_Days');
-xw.text('TODO value');
-xw.endElement('Number_of_Days');
-xw.endDocument({ pretty: true});
-
-console.log(xw.toString());
-/**
-var jsonxml = require('jsontoxml');
-
-var xml = jsonxml({
-	node:'text content',
-	parent:[
-		{name:'taco',text:'beef taco',children:{salsa:'hot!'}},
-		{name:'taco',text:'fish taco',attrs:{mood:'sad'},children:[
-			{name:'salsa',text:'mild'},
-			'hi',
-			{name:'salsa',text:'weak',attrs:{type:2}}
-		]},
-		{name:'taco',attrs:'mood="party!"'}
-	],
-	parent2:{
-		hi:'is a nice thing to say',
-		node:'i am another not special child node',
-		date:function(){
-			return (new Date())+'';
-		}
-	}
-}, {header: true, prettyPrint: true});
-*/
-var xml = jsonToXml({
-	node:'text content',
-	parent:[
-		{name:'taco',text:'beef taco',children:{salsa:'hot!'}},
-		{name:'taco',text:'fish taco',attrs:{mood:'sad'},children:[
-			{name:'salsa',text:'mild'},
-			'hi',
-			{name:'salsa',text:'weak',attrs:{type:2}}
-		]},
-		{name:'taco',attrs:'mood="party!"'}
-	],
-	parent2:{
-		hi:'is a nice thing to say',
-		node:'i am another not special child node',
-		date:function(){
-			return (new Date())+'';
-		}
-	}
-})
-
-console.log(xml);
-
-function jsonToXml(jsonObject) {
-	var jsonxml = require('jsontoxml');
-	var xml = jsonxml(jsonObject, {header: true, prettyPrint: true});
-	
-	return xml;
+var stringBaseXmlFileForFet = jsonToXml([
+   {name : 'fet', attrs : {version: '5.30.3'}, children : [
+       {name : 'Intsitution_Name', text : 'yolo'},
+       {name : 'Comments', text : ''},
+       {name : 'Days_List', children : [
+           {name: 'Number_of_Days', text: '6'},
+           {name: 'Day', children: {
+               Name : 'Lundi'
+           }},
+           {name: 'Day', children: {
+               Name: 'Mardi'
+           }},
+       ]},
+       {name : 'Hour_List', children :[        ]},
+       {name : 'Subjects_List', children :[        ]},
+       {name : 'Activity_Tags_List', children :[        ]}    ]}
+]);console.log(stringBaseXmlFileForFet);function addDay(stringDay, jsonObject) {
+    jsonObject
+}function jsonToXml(jsonObject) {
+    var jsonxml = require('jsontoxml');
+    var xml = jsonxml(jsonObject, {xmlHeader: true, prettyPrint: true});
+    
+    return xml;
+}var jsonObjectEntry = {
+   teacherList : [
+       {firstname : 'Premier', lastname : 'Second', subjectlist : [
+           'Maths',
+           'Physique'
+       ], groupsList : [
+           'Classe A',
+           'Classe B'
+       ]},
+       {firstname : 'Premier2', lastname : 'Second2', subjectlist : [
+           'Maths',
+           'Physique'
+       ], groupsList : [
+           'Classe A',
+           'Classe B'
+       ]}
+   ],
+   subjectsList : [
+       'Maths',
+       'Physique'
+   ],
+   roomTypeList : [
+       'TP',
+       'Musique',
+       'Art',
+       'Normal'
+   ],
+   roomList : [
+       {roomName : '311', roomType : 'Normal', capacity : 45}
+   ],
+   grade : [
+       ''
+   ]
 }
