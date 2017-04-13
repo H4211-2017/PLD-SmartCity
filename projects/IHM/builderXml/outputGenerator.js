1dc8dd3c651926670b4577c31b6f941cafa193f0
Pyhro36 <pylou_11@hotmail.fr>
2017-04-13 18:32:36 +0200

    outputGenerator

--- projects/IHM/builderXml/outputGenerator.js
+++ projects/IHM/builderXml/outputGenerator.js
@@ -1,77 +1,71 @@
-var builder = require('./builder.js');
-
-console.log(builder);
-
 var outputJsonGenerator = {
 
-	outputJsonObject: [
-	   	{name : 'fet', attrs : {version : '5.30.3'}, children : [
-		   	{name : 'Intsitution_Name', text : 'yolo'},
-		   	{name : 'Comments', text : ''},
-		   	{name : 'Days_List', children : [
-		       	{name : 'Number_of_Days', text : '6'},
-		       	{name : 'Day', children : {
-		           Name : 'Lundi'
-		       	}},
-		       	{name : 'Day', children: {
-		           Name : 'Mardi'
-			   	}},
-		   	]},
-		   	{name : 'Hour_List', children : []},
-		   	{name : 'Subjects_List', children : []},
-		   	{name : 'Activity_Tags_List', children : []},
-		   	{name : 'Teacher_List', children : []}
-		]}
-	],
+    outputJsonObject: [
+        {name : 'fet', attrs : {version : '5.31.4'}, children : [
+            {name : 'Intsitution_Name', text : 'institution par defaut'},
+            {name : 'Comments', text : 'commentaires par defaut'},
+            {name : 'Days_List', children : [
+                {name : 'Number_of_Days', text : 0}
+            ]},
+            {name : 'Hour_List', children : [
+                {name : 'Number_of_Hours', text : 0}
+            ]},
+            {name : 'Subjects_List', children : []},
+            {name : 'Activity_Tags_List', children : []},
+            {name : 'Teacher_List', children : []},
+            {name : 'Students_List', children : []}
+        ]}
+    ],
 
     addDay : function(stringDay) {
-        var hour = {name : 'Hour', children : {
+        var day = {name : 'Day', children : {
             Name : stringDay
         }};
 
-        this.outputJsonObject[0].children[2].children.push(hour);
+        var tableDayList = this.outputJsonObject[0].children[2].children;
+        tableDayList[0].text++; //number of days
+        tableDayList.push(day);
     },
 
-	addHour : function(stringHour) {
-		var hour = {name : 'Hour', children : {
-			Name : stringHour
-		}};
-
-		this.outputJsonObject[0].children[3].children.push(hour);
-	},
-
-    addSubject : function(stringSubject) {
+    addHour : function(stringHour) {
         var hour = {name : 'Hour', children : {
-            Name : stringSubject
+            Name : stringHour
         }};
 
-        this.outputJsonObject[0].children[4].children.push(hour);
+        var tableHourList = this.outputJsonObject[0].children[3].children;
+        tableHourList[0].text++; //number of days
+        tableHourList.push(hour);
     },
 
-    addActivityTags : function(stringActivityTags) {
-        var hour = {name : 'Hour', children : {
-            Name : stringSubject
+    addSubject : function(stringSubjectName, stringComments = '\0') {
+        var subject = {name : 'Subject', children : {
+            Name : stringSubjectName,
+            Comments : stringComments
         }};
 
-        this.outputJsonObject[0].children[5].children.push(hour);
+        var tableSubjectList = this.outputJsonObject[0].children[4].children;
+        tableSubjectList.push(subject);
     },
 
-    addTeacher : function(stringTeacher) {
+    addActivityTags : function(stringActivityTagName, stringComments = '\0') {
+        var activityTag = {name : 'Activity_Tag', children : {
+            Name : stringActivityTagName,
+            Comments : stringComments
+        }};
 
+        var tableActivityTagList = this.outputJsonObject[0].children[5].children;
+        tableActivityTagList.push(activityTag);
+    },
+
+    addTeacher : function(stringTeacherName, numberTargetOfHours = 0, stringComments = '\0') {
+        var teacher = {name : 'Teacher', children : [
+            {name : 'Name', text : stringTeacherName},
+            {name : 'Target_Number_of_Hours', text : numberTargetOfHours},
+            {name : 'Qualified_Subjects', children : []},
+            {name : 'Comments', text : stringComments}
+        ]};
+
+        var tableActivityTagList = this.outputJsonObject[0].children[6].children;
+        tableActivityTagList.push(teacher);
     }
 };
-
-outputJsonGenerator.addHour('8.35 - 9.30');
-outputJsonGenerator.addDay('Lundi');
-outputJsonGenerator.addSubject('Mathematiques');
-
-var jsonToXml = function (jsonObject) {
-    var jsonxml = require('jsontoxml');
-    var xml = jsonxml(jsonObject, {xmlHeader: true, prettyPrint: true});
-
-    return xml;
-};
-
-var toXml = jsonToXml(outputJsonGenerator.outputJsonObject);
-
-console.log(toXml);
