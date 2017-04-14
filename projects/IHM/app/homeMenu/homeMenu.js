58206c1c9c5297966f3d9c7b1ed158d6802d77bd
Sbelletier <sbelletier@gmail.com>
2017-04-14 16:08:18 +0200

    Recuperation du contenu textuel d'un fichier sans traitement
    Ca casse salement le CSS par contre pour l'instant

--- projects/IHM/app/homeMenu/homeMenu.js
+++ projects/IHM/app/homeMenu/homeMenu.js
@@ -3,6 +3,16 @@
  */
 'use strict';
 
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  console.log("File API support enabled on this browser")
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
+
+//
+
+
 angular.module('myApp.homeMenu', ['ngRoute'])
   
   .config(['$routeProvider', function($routeProvider) {
@@ -12,6 +22,35 @@ angular.module('myApp.homeMenu', ['ngRoute'])
     });
   }])
   
-  .controller('homeMenuCtrl', [function() {
-  
+
+  .controller('homeMenuCtrl', ["$scope", function($scope) {
+	  
+	  // Setup the file listener.
+		var loadButton = $('#fileUploader');
+		console.log(loadButton)
+		loadButton.change(function (evt) {
+			console.log("fichier detecté");
+			var file = evt.target.files[0];
+			var reader = new FileReader();
+			
+			//defining callback
+			reader.onload = function (finished_event) {
+				console.log(finished_event.target.result)
+				//TODO do things with caught file on the server
+			};
+			reader.readAsText(file)
+		});
+	  //
+	  $scope.save = function() {
+			console.log("Save Pressed")
+	  };
+	  
+	  $scope.load = function() {
+			console.log("Load Pressed")
+	  };
   }]);
+  
+  
+
+
+
