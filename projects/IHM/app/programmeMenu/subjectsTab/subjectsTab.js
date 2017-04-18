a8154f7c2ea57d4f0de9348116baea406b50ecd4
frobion <francois.robion@insa-lyon.fr>
2017-04-18 18:38:56 +0200

    Add roomType to subjectTab (Currently hard coded)

--- projects/IHM/app/programmeMenu/subjectsTab/subjectsTab.js
+++ projects/IHM/app/programmeMenu/subjectsTab/subjectsTab.js
@@ -2,18 +2,18 @@
 
 angular.module('myApp.subjectsTab', ['ngRoute'])
 
-  .config(['$routeProvider', function($routeProvider) {
+  .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/subjectsTab', {
       templateUrl: 'subjectsTab/subjectsTab.html',
       controller: 'subjectsTabCtrl'
     });
   }])
 
-  .controller('subjectsTabCtrl', ['$scope', function($scope) {
+  .controller('subjectsTabCtrl', ['$scope', function ($scope) {
 
-    $scope.openTab = function(nom) {
+    $scope.openTab = function (nom) {
       $scope.page = nom;
-      console.log("changement page "+$scope.page);
+      console.log("changement page " + $scope.page);
     };
 
     $scope.subjects = [];
@@ -26,7 +26,7 @@ angular.module('myApp.subjectsTab', ['ngRoute'])
       if ($scope.inputValue !== null && $scope.inputValue !== "") {
         //looking if the subjectalready exists, isn't case sensitive
         for (var i = 0; i < $scope.subjects.length; i++) {
-          if ($scope.subjects[i].toUpperCase() === $scope.inputValue.toUpperCase()) {
+          if ($scope.subjects[i].subjectName.toUpperCase() === $scope.inputValue.toUpperCase()) {
             isDuplicate = true;
             break;
           }
@@ -34,20 +34,22 @@ angular.module('myApp.subjectsTab', ['ngRoute'])
 
         if (isDuplicate === false) {
           var roomType = $scope.inputValue;
-          $scope.subjects.unshift(roomType);
-          $scope.inputValue = null;
+          $scope.subjects.unshift(
+            {
+              subjectName: $scope.inputValue,
+              roomType: $scope.subjectSelected
+            });
+          $scope.inputValue = "";
         }
         else {
           $scope.inputValue = "";
-          alert("Ce type de salle existe déjà");
+          alert("Cette matière existe déjà");
         }
 
       }
     };
 
     $scope.removeSubject = function (rowToDelete) {
-      console.log(rowToDelete);
-
       //We splice behaves weirdly on end of array, so we use pop instead.
       if (rowToDelete === $scope.subjects.length) {
         $scope.subjects.pop();
