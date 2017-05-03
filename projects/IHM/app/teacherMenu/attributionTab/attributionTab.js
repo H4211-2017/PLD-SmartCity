e54b8db397851bebe174205ed42b6f9de1886f62
frobion <francois.robion@insa-lyon.fr>
2017-05-03 11:03:12 +0200

    tmp commit to pull

--- projects/IHM/app/teacherMenu/attributionTab/attributionTab.js
+++ projects/IHM/app/teacherMenu/attributionTab/attributionTab.js
@@ -12,6 +12,9 @@ angular.module('myApp.teacherMenu.attributionTab', ['ngRoute', 'myApp.dataFactor
 
 .controller('attributionTabCtrl', ['$scope', 'dataFactory', function ($scope, dataFactory) {
 
+  console.log("attributionTab::ctrl : attribution : ");
+  printObjectCaracteristic(dataFactory.getAttributionArray(), ['$$hashKey']);
+
   $scope.teacherArray = dataFactory.getTeacherArray();
   $scope.subjectArray = dataFactory.getSubjectsArray().sort(function (a, b) {
       return a.name.localeCompare(b.name);
@@ -19,21 +22,22 @@ angular.module('myApp.teacherMenu.attributionTab', ['ngRoute', 'myApp.dataFactor
   $scope.classeArray = dataFactory.getClassesArray();
   
   $scope.attributionArray = dataFactory.getAttributionArray();
-  
-  $scope.currentFirstName = "";
-  $scope.currentLastName = "";
-  $scope.currentSubjectsSelected = [];
 
-  $scope.addTeacher = function() {
-    if ($scope.currentFirstName.trim() && $scope.currentLastName.trim() && $scope.currentSubjectsSelected.length > 0) {
-      if (!dataFactory.addTeacher($scope.currentFirstName.trim(), $scope.currentLastName.trim(), $scope.currentSubjectsSelected)) {
-        alert("Un enseignant avec ce nom et ce prénom existe déjà");
-      }
-      $scope.currentFirstName = "";
-      $scope.currentLastName = "";
-      $scope.currentSubectsSelected = [];
-    }
-  };
+
+  // $scope.currentFirstName = "";
+  // $scope.currentLastName = "";
+  // $scope.currentSubjectsSelected = [];
+  //
+  // $scope.addTeacher = function() {
+  //   if ($scope.currentFirstName.trim() && $scope.currentLastName.trim() && $scope.currentSubjectsSelected.length > 0) {
+  //     if (!dataFactory.addTeacher($scope.currentFirstName.trim(), $scope.currentLastName.trim(), $scope.currentSubjectsSelected)) {
+  //       alert("Un enseignant avec ce nom et ce prénom existe déjà");
+  //     }
+  //     $scope.currentFirstName = "";
+  //     $scope.currentLastName = "";
+  //     $scope.currentSubectsSelected = [];
+  //   }
+  // };
 
 
 }
